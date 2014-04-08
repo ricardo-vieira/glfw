d8c1575e048604246e91918bdf2a781172a69626
Camilla Berglund <elmindreda@elmindreda.org>
2014-04-08 21:16:48 +0200

    Consume WM_ERASEBKGND.

--- src/win32_window.c
+++ src/win32_window.c
@@ -732,6 +732,11 @@ static LRESULT CALLBACK windowProc(HWND hWnd, UINT uMsg,
             break;
         }
 
+        case WM_ERASEBKGND:
+        {
+            return TRUE;
+        }
+
         case WM_SETCURSOR:
         {
             if (_glfw.focusedWindow == window && LOWORD(lParam) == HTCLIENT)
